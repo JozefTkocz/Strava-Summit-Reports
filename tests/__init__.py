f212d8bf6ac71e02580b52492d527c09acad862f
Jozef Tkocz <jozef.tkocz@uk.zurich.com>
2022-07-14 16:51:16 +0100

    fix typo

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ import sys, os
 # append src directory to path to allow tests to run locally
 test_dir = os.path.dirname(__file__)
 src_dir = os.path.join(os.pardir, 'src')
-sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+sys.path.insert(0, os.path.abspath(os.path.join(test_dir, src_dir)))
