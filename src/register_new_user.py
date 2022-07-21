5961740dafe39251ff7ceff5e59039d289229ff1
Jozef Tkocz <jozef.tkocz@uk.zurich.com>
2022-07-21 16:10:30 +0100

    set limit to number of registered users at 5

--- src/register_new_user.py
+++ src/register_new_user.py
@@ -5,28 +5,34 @@ from typing import Dict, Union
 
 logging.getLogger().setLevel(logging.INFO)
 
+MAX_REGISTERED_USERS = 5
+
 
 def lambda_handler(event, context):
     strava_client = create_strava_client_from_env()
-    lambda_url = 'https://' + event['headers'].get('host') + '/'
 
+    if strava_client.authorisation.token_cache.total_tokens >= MAX_REGISTERED_USERS:
+        return {'statusCode': 400,
+                'body': json.dumps('Maximum registered users limit exceeded.')}
+
+    lambda_url = 'https://' + event['headers'].get('host') + '/'
     authorisation_code = parse_code_from_query_string(event)
 
     if authorisation_code is None:
         scope = 'read_all,activity:read_all,activity:write'
         code_request_url = strava_client.authorisation.generate_authorisation_url(scope=scope,
                                                                                   redirect_uri=lambda_url)
-        return {'statusCode': 400,
+        return {'statusCode': 200,
                 'body': json.dumps({'Authorisation url': code_request_url})}
     else:
         try:
             athlete_id = strava_client.authorisation.post_athlete_auth_code(authorisation_code)
             return {'statusCode': 200,
                     'body': json.dumps(f'Successfully registered athlete with ID {athlete_id}')}
-        except:
+        except Exception:
             logging.exception('Unable to authorise athlete')
             return {'statusCode': 400,
-                    'body': json.dumps(f'Unable to register athlete.')}
+                    'body': json.dumps('Unable to register athlete.')}
 
 
 def parse_code_from_query_string(event: Dict) -> Union[int, None]:
